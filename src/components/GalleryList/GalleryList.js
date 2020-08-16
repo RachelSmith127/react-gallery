d828b04e07f9d80009b24ffb9a92452bf60ccbe8
RachelSmith127 <62163543+RachelSmith127@users.noreply.github.com>
2020-08-16 15:14:29 -0500

    Notes added to GalleryList

--- src/components/GalleryList/GalleryList.js
+++ src/components/GalleryList/GalleryList.js
@@ -1,3 +1,4 @@
+//basic setup 
 import React, { Component } from 'react';
 import GalleryItem from '../GalleryItem/GalleryItem.js'
 
@@ -7,12 +8,12 @@ class GalleryList extends Component {
       console.log(this.props.listOfImages)
         return(
             <>
+            {/* this is our loop that loops over all of our gallery images and helps us count likes */}
                 {this.props.listOfImages.map((image) =>  { 
                     return (
                         <GalleryItem key={image.id} image={image} addLike={this.props.addLike}/>
                     )
                 })}
-                {/* <img src="images/goat_small.jpg"/> */}
                
             </>
         )
